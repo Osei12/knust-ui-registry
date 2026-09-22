142c8ea48d4a95af61ab1679d351d0a1c27073f1
oseiadu6 <oa.kusi@knust.edu.gh>
2026-09-22 16:44:07 +0000

    Refactor button component: move logic to Button.razor.cs and update manifest

---
+++ components/button/Button.razor.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel;
+
+namespace Registry.components.button;
+
+public class Button:ComponentBase
+{
+
+     [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter]
+    public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+    [Parameter]
+    public bool Disabled { get; set; }
+
+    [Parameter]
+    public string Type { get; set; } = "button";
+
+    [Parameter]
+    public string? Class { get; set; }
+
+    private string CssClass =>
+        $"knust-ui-button {Class}".Trim();
+
+}
