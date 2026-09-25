13ad71d1ab6b58263f78a8cd7f6c49f3e62d8380
oseiadu6 <oa.kusi@knust.edu.gh>
2026-09-25 16:50:19 +0000

    feat: Refactor Button component to support additional attributes and customizable variants

--- components/button/Button.razor.cs
+++ components/button/Button.razor.cs
@@ -1,28 +1,65 @@
 using System.ComponentModel;
 using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Components.Web;
 
 namespace __MYUI_NAMESPACE__;
 
-public partial class Button:ComponentBase
+
+public partial class Button : ComponentBase
 {
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+    [Parameter] public ButtonVariant Variant { get; set; } = ButtonVariant.Primary;
+    [Parameter] public ButtonSize Size { get; set; } = ButtonSize.Default;
+    [Parameter] public bool IsRounded { get; set; } = false;
+    [Parameter] public string? Class { get; set; }
+    [Parameter(CaptureUnmatchedValues = true)] public Dictionary<string, object>? AdditionalAttributes { get; set; }
 
-     [Parameter]
-    public RenderFragment? ChildContent { get; set; }
+    protected string ClassName => new CssBuilder("inline-flex gap-3 items-center justify-center text-sm font-normal transition-all active:scale-95 duration-100 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background")
+        .AddClass(IsRounded ? "rounded-full" : "rounded-xl")
+        .AddClass(VariantClasses[Variant])
+        .AddClass(SizeClasses[Size])
+        .AddClass(Class)
+        .Build();
 
-    [Parameter]
-    public EventCallback<MouseEventArgs> OnClick { get; set; }
+    private static readonly Dictionary<ButtonVariant, string> VariantClasses = new()
+    {
+        { ButtonVariant.Primary, "bg-primary text-white hover:bg-primary/90" },
+        { ButtonVariant.Destructive, "bg-status-error text-white hover:bg-status-error/90" },
+        { ButtonVariant.Outline, "border border-primary hover:bg-accent hover:text-accent-foreground" },
+        { ButtonVariant.Secondary, "bg-secondary text-secondary-foreground dark:text-neutral-600 hover:bg-secondary/80" },
+        { ButtonVariant.Ghost, "hover:bg-gray-50/40 hover:text-accent-foreground" },
+        { ButtonVariant.Link, "underline-offset-4 hover:underline text-primary" }
+    };
 
-    [Parameter]
-    public bool Disabled { get; set; }
+    private static readonly Dictionary<ButtonSize, string> SizeClasses = new()
+    {
+        { ButtonSize.Xs, "h-7 px-2 text-xs" },
+        { ButtonSize.Sm, "h-9 px-3" },
+        { ButtonSize.Default, "h-10 py-2 px-4" },
+        { ButtonSize.Lg, "h-11 px-8 text-base" },
+        { ButtonSize.Xl, "h-12 px-10 text-lg" },
+        { ButtonSize.Icon, "h-10 w-10" }
+    };
 
-    [Parameter]
-    public string Type { get; set; } = "button";
+    public enum ButtonVariant
+    {
+        Primary,
+        Destructive,
+        Outline,
+        Secondary,
+        Ghost,
+        Link
+    }
 
-    [Parameter]
-    public string? Class { get; set; }
-
-    private string CssClass =>
-        $"knust-ui-button {Class}".Trim();
-
-}
+    public enum ButtonSize
+    {
+        Xs,
+        Sm,
+        Default,
+        Lg,
+        Xl,
+        Icon
+    }
+    }
+    
